81e0c8c0e39e5e9707ea346ae75c08224cf3ae6b
AugustinLambotte <augustin.lambotte@gmail.com>
2023-04-07 12:03:28 +0200

    Extracting data

---
+++ 1.py
@@ -0,0 +1,29 @@
+import xarray as xr 
+from datetime import datetime, date, timedelta
+import numpy as np
+
+def extract_data(file = "./Data/osisaf_nh_sie_monthly.nc", dtype = "dict"):
+    """ Extract september Sea Ice extent (sie) from 1979 to 2022. can return the data in two different parts depending on dtype parameter:
+    dtype = "dict" return a dictionnary {'1979': 7.556, '1980': 8.244,...} 
+    dtype = "array" return a np.array [7.556, 8.244, ...]
+    """
+    ds = xr.open_dataset(file)
+    if dtype == "dict":
+        september_sie = {}
+        for year in range(1979,2022):
+            september_sie[f"{year}"] = float(ds['sie'].sel(time = datetime(year,9,16)))
+        
+    elif dtype == "array":
+        september_sie = []
+        for year in range(1979,2022):
+            september_sie.append(float(ds['sie'].sel(time = datetime(year,9,16))))
+        september_sie = np.array(september_sie)
+    else:
+        print("! Incorrect data type !")
+        ds.close()
+        return 0
+    ds.close()
+    return september_sie
+
+s = extract_data(dtype = "array")
+print(s)
