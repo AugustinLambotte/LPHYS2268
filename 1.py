8549f64797f25d99b0c877daf774e44b76934dea
AugustinLambotte <augustin.lambotte@gmail.com>
2023-04-08 13:35:37 +0200

    Add Plot_APF(). A function based on APF() which compute forecasting for each year based on the precedents years and plotting it upon the observed data

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import xarray as xr
 from datetime import datetime, date, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
+import numpy.ma as ma
 
 def extract_data(file = "./Data/osisaf_nh_sie_monthly.nc", dtype = "array", month = 9):
     """ Extract Sea Ice extent (sie) from 1979 to 2022 for the month "month".
@@ -58,15 +58,30 @@ def trend_line_coeff(sept_sie):
     return a,b, forecast
 
 def APF(year, may_sie, sept_sie):
-    """ Anomaly Persistence forecast"""
-    nb_year = year - 1979
-    obs_sept_mean = np.mean([sept_sie[year] for year in range(1979,year)])
-    obs_may_mean = np.mean([may_sie[year] for year in range(1979,year)])
-    
-    Forecast_mean = obs_sept_mean + (may_sie[year] - obs_may_mean)     #Eq (2) in instruction
-    Forecast_var = (1/nb_year) * (np.var([may_sie[year] for year in range(1979,year)]) + np.var([sept_sie[year] for year in range(1979,year)]))    #Eq(3) in instruction
-    return Forecast_mean, Forecast_var
+    """ Anomaly Persistence forecast
+    ma.masked_invalid([array]) is used because some data are missing (NaN). masking them allowed the var and mean operations to handle it.
+    """
 
+    nb_year = year - 1979
+    obs_sept_mean = np.mean(ma.masked_invalid([sept_sie[year] for year in range(1979,year)]))
+    obs_may_mean = np.mean(ma.masked_invalid([may_sie[year] for year in range(1979,year)]))
+    Forecast_mean = obs_sept_mean + (may_sie[year] - obs_may_mean)     #Eq (2) in instruction
+    Forecast_var = (1/nb_year) * (np.std(ma.masked_invalid([may_sie[year] for year in range(1979,year)])) + np.std(ma.masked_invalid([sept_sie[year] for year in range(1979,year)])))    #Eq(3) in instruction
+    return [Forecast_mean, Forecast_var]
+
+def Plot_APF(sept_sie, may_sie):
+    #APF_mean = [APF(year, may_sie, sept_sie)[0] for year in range(1981,2022)]
+    #print(APF(2000, may_sie, sept_sie))
+    #print([APF(year, may_sie, sept_sie)[1] for year in range(1981,2022)])
+    plt.plot([year for year in range(1979,2022)], [sept_sie[year] for year in range(1979,2022)], label = 'Observed')
+    plt.errorbar([year for year in range(1981,2022)], [APF(year, may_sie, sept_sie)[0] for year in range(1981,2022)], [2*APF(year, may_sie, sept_sie)[1] for year in range(1981,2022)],linestyle='None', marker='^', label = 'Forecasted')
+    plt.grid()
+    plt.legend()
+    plt.ylabel('SIE [1e6 km^2]')
+    plt.title("Sea ice extent in September. Forecasted values with 2 times std.")
+    plt.show()
 sept_sie = extract_data(dtype="dict", month=9)
 may_sie = extract_data(dtype="dict", month=5)
 a,b, forecast = trend_line_coeff(extract_data(dtype="dict"))
+Plot_APF(sept_sie,may_sie)
+#plot_data(may_sie,a,b)
