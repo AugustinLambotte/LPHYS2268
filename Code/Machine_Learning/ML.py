a5d0adef26e87baf427964b75e337f983316d496
AugustinLambotte <augustin.lambotte@gmail.com>
2023-05-18 14:59:38 +0200

    add plsm data

--- Code/Machine_Learning/ML.py
+++ Code/Machine_Learning/ML.py
@@ -83,7 +83,7 @@ class NN():
 
     ######## - LPY - #######
      
-    def formating_data_LPY(self, test_size = 0.1):
+    def formating_data_LPY(self, test_size = 0.001):
         """
             Format data to be in the right shape for LPY events prediction.
             test_size can be adjust: it is the proportion of the dataset which will not
@@ -170,7 +170,7 @@ class NN():
         # Training
         history = self.model_SIEFrcst.fit(self.x_train, self.y_train, epochs=epochs, batch_size=128)
         if save:
-            model_name = "NN2"
+            model_name = "Model"
             self.model_SIEFrcst.save(model_name)
         
     def test_SIEfrcst(self):
@@ -207,130 +207,11 @@ class NN():
         plt.title('Comparison btwn forecasted and true September SIE, \n based on Neural Network model and data coming from PlaSim run.')
         plt.show()
 
-    def form(self, test_size = 0.01):
-        
-        
-        # Normalization of input datas
-        sc = StandardScaler()
-        x = sc.fit_transform(self.x)
-        # Splitting our data set in training and testing parts
-        self.x_train,self.x_test,self.y_train,self.y_test = train_test_split(x,self.y,test_size = test_size)
 
-    def constr(self, epochs = 60,save = False):
-        """
-            Construct the neural network and train him to predict sept_SIE
-        """
-        def normal_distrib_loss(y_true, y_pred):
-            """
-            Normal distribution loss function.
-            Assumes tensorflow backend.
-            
-            Parameters
-            ----------
-            y_true : tf.Tensor
-                Ground truth values of predicted variable.
-            y_pred : tf.Tensor
-                n and p values of predicted distribution.
-                
-            Returns
-            -------
-            nll : tf.Tensor
-                Negative log likelihood.
-            """
-            import tensorflow as tf
-            # Separate the parameters
-            mu, sigma = tf.unstack(y_pred, num=2, axis=-1)
-            
-            # Add one dimension to make the right shape
-            mu = tf.expand_dims(mu, -1)
-            sigma = tf.expand_dims(sigma, -1)
-            
-            # Calculate the negative log likelihood
-            dist = tfp.distributions.Normal(loc=mu, scale=sigma)
-            loss = tf.reduce_mean(-dist.log_prob(y_true))   
-             
-
-            return loss
-        
-        def Gaussian_layer(x):
-            """
-            Lambda function for generating gaussian parameters
-            n and p from a Dense(2) output.
-            Assumes tensorflow 2 backend.
-            
-            
-            Parameters
-            ----------
-            x : tf.Tensor
-                output tensor of Dense layer
-                
-            Returns
-            -------
-            out_tensor : tf.Tensor
-                
-            """
-            import numpy as np
-            import tensorflow as tf
-            from tensorflow import keras
-            # Get the number of dimensions of the input
-            num_dims = len(x.get_shape())
-            
-            # Separate the parameters
-            mu,sigma = tf.unstack(x, num=2, axis=-1)
-            
-            # Add one dimension to make the right shape
-            mu = tf.expand_dims(mu, -1)
-            sigma = tf.expand_dims(sigma, -1)
-                
-            # Apply a softplus to make positive
-            mu = tf.keras.activations.softplus(mu)
-
-            sigma = tf.keras.activations.sigmoid(sigma/250000)*250000
-
-            # Join back together again
-            out_tensor = tf.concat((mu, sigma), axis=num_dims-1)
-
-            return out_tensor
-        
-        # Contruction
-        
-        self.model_SIEFrcst = Sequential()
-        self.model_SIEFrcst.add(Dense(500, input_dim=len(self.x[0]), activation='relu')) 
-        self.model_SIEFrcst.add(Dense(300, activation='relu'))
-        self.model_SIEFrcst.add(Dense(100, activation='relu'))
-        self.model_SIEFrcst.add(Dense(2, activation = 'relu'))
-        self.model_SIEFrcst.add(Lambda(Gaussian_layer))
-
-        
-        self.model_SIEFrcst.compile(loss=normal_distrib_loss, optimizer= 'Adam')
-        # Training
-        history = self.model_SIEFrcst.fit(self.x_train, self.y_train, epochs=epochs, batch_size=128)
-        if save:
-            model_name = "NN2"
-            self.model_SIEFrcst.save(model_name)
-
-    def test(self):
-        y_pred = self.model_SIEFrcst.predict(self.x_test)
-        print(self.y_test[0])
-        plt.hist(np.random.normal(loc = y_pred[0,0], scale = y_pred[0,1],size = 100000),bins = 50,label = f'std = {y_pred[0,1]}')
-        plt.plot([int(self.y_test[0]),int(self.y_test[0])],[0,7000])
-        plt.legend()
-        plt.show()
-        # Plot
-        
-        plt.scatter(y_pred[:,0],self.y_test)
-        plt.errorbar(y_pred[:,0],self.y_test,xerr=y_pred[:,1], linestyle="None")
-
-        plt.xlabel('predicted SIE [1e7 km^2]')
-        plt.ylabel('True SIE [1e7 km^2]')
-        plt.grid()
-        plt.title('Comparison btwn forecasted and true September SIE, \n based on Neural Network model and data coming from PlaSim run.')
-        
-        plt.show()
 NN = NN()
-NN.form()
-NN.constr(epochs = 100,save = True)
-NN.test()
+NN.formating_data_SIEfrcst()
+NN.construct_SIEfrcst(epochs = 100,save = True)
+NN.test_SIEfrcst()
 #R.formating_data_SIE_Frcst()
 
 
